8eee84c1463cbbb264e230a3d728d5096fad6f88
junglefive <2282918128@qq.com>
2017-08-17 20:40:09 +0800

    pycharm

---
+++ chipsea/parseJson.py
@@ -0,0 +1,16 @@
+import json
+try:
+    print('parsing file: sfr.json')
+    with open('sfr.json','r') as json_file:
+        print('content of file is：')
+        data = json.load(json_file)
+        json_file.close()
+        print(type(data))
+        print('\nname:',data['sfr_name'],'\nadress:',data['adress'],'\nvalue:',data['write_value'])
+        with
+except e:
+    print('erro', str(e))
+finally:
+    input()
+
+
