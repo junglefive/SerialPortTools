af331d370e08a3014962625906f3481e64e0227d
junglefive <2282918128@qq.com>
2017-09-05 20:26:08 +0800

    UI_work

--- CSM3510_Helper/CSM3510_Helper.py
+++ CSM3510_Helper/CSM3510_Helper.py
@@ -1,31 +1,57 @@
-import sys
+import sys,time
 from main_window_ui import *
+from PyQt5.QtCore import *
+
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self,csm_helper):
+        super(MyApp,self).__init__()
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
+        self.csm_helper = csm_helper
         self.setupUi(self)
         # self.resetButton.clicked.connect(self.reset_button_clicked)
+        self.pushButtonReset.clicked.connect(self.reset_button_clicked)
+        self.csm_helper.sinstr.connect(self.sin_str_call)
 
-
+    def sin_str_call(self, info):
+        self.set_line_result(info)
     def reset_button_clicked(self):
         pass
+        self.set_line_result("FAIL")
         # self.plainTextEdit.setPlainText('修改成功')
+    def set_line_result(self, str_result):
+        self.lineEdit_result.setText(str_result)
 
-def initiate_app():
-    try:
-        pass
-        app = QtWidgets.QApplication(sys.argv)
-        window = MyApp()
-        window.show()
-        log = open("./log.md", "w+")
-    except Exception as e:
-        log.write(str(e))
-        log.close()
-        print(str(e))
-    finally:
-        sys.exit(app.exec_())
+
+
+class CSM3510_Helper(QThread):
+      sinstr = pyqtSignal(str)
+      def  __init__(self):
+          super(CSM3510_Helper, self).__init__()
+      def run(self):
+          while True:
+              time.sleep(2)
+              self.sinstr.emit("FAIL")
+              time.sleep(2)
+              self.sinstr.emit("PASS")
+
+      def initiate_app(self):
+          try:
+              pass
+              app = QtWidgets.QApplication(sys.argv)
+              window = MyApp(self)
+              window.show()
+              pass
+          except Exception as e:
+              print(str(e))
+          finally:
+              sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    initiate_app()
+    csm_helper = CSM3510_Helper()
+    csm_helper.start()
+    csm_helper.initiate_app()
+
+
+
 
