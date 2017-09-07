175a401b26b22131a64f25dc1a71add364b50d6a
junglefive <2282918128@qq.com>
2017-09-07 20:47:23 +0800

    更新了功能，增加测试代码

--- CSM3510_Helper/CSM3510_Helper.py
+++ CSM3510_Helper/CSM3510_Helper.py
@@ -3,9 +3,9 @@ from main_window_ui import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from serial import *
 from chipsea_tools import *
-
+from serial.tools.list_ports import *
+import datetime
 
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self,csm_helper):
@@ -14,12 +14,18 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.csm_helper = csm_helper
         self.setupUi(self)
+        # function initial
+        self.init_component_content()
         #initiate
         self.btn_reset_result.clicked.connect(self.reset_button_clicked)
         self.btn_csm3510_setting.clicked.connect(self.csm3510_btn_setting_clicked)
         self.btn_cc2640_setting.clicked.connect(self.cc2640_btn_setting_clicked)
         self.btn_printer_setting.clicked.connect(self.printer_btn_setting_clicked)
         self.btn_currenter_setting.clicked.connect(self.currenter_btn_setting_clicked)
+        self.checkBox_csm3510.clicked.connect(self.sin_checked_csm3510)
+        self.checkBox_cc2640.clicked.connect(self.sin_checked_cc2640)
+        self.checkBox_printer.clicked.connect(self.sin_checked_printer)
+        self.checkBox_currenter.clicked.connect(self.sin_checked_currenter)
         #设备
         self.btn_csm3510_autodetect.clicked.connect(self.btn_csm3510_autodetect_call)
         self.btn_cc2640_autodetect.clicked.connect(self.btn_cc2640_autodetect_call)
@@ -29,36 +35,130 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.csm_helper.sin_str.connect(self.sin_str_call)
         self.csm_helper.sin_dis_str.connect(self.sin_dis_str_call)
         self.csm_helper.sin_log_str.connect(self.sin_log_str_call)
+        self.csm_helper.sin_result_int.connect(self.sin_result_int_call)
+        #else
+        self.textBrowser_help.setSource(QUrl("help.html"))
+        self.textBrowser_reuslt.setSource(QUrl("waitting.html"))
+
+
+    def init_component_content(self):
+        # port
+        list_port = serial.tools.list_ports.comports()
+        for name in list_port:
+            self.csm3510_comboBox_port.addItem(name[0])
+            self.cc2640_comboBox_port.addItem(name[0])
+            self.currenter_comboBox_port.addItem(name[0])
+            self.printer_comboBox_port.addItem(name[0])
+        list_baudrate = [9600, 1200,4800,14400,19200,28800,57600,115200]
+        for bd in list_baudrate:
+            self.csm3510_comboBox_baudrate.addItem(str(bd))
+            self.cc2640_comboBox_baudrate.addItem(str(bd))
+            self.currenter_comboBox_baudrate.addItem(str(bd))
+            self.printer_comboBox_baudrate.addItem(str(bd))
+        list_dataBit = [8,5,6,7]
+        for len in list_dataBit:
+            self.csm3510_comboBox_databits.addItem(str(len))
+            self.cc2640_comboBox_databits.addItem(str(len))
+            self.currenter_comboBox_databits.addItem(str(len))
+            self.printer_comboBox_databits.addItem(str(len))
+
+    def set_green_text(self, comp):
+        palette = QPalette()
+        palette.setColor(QPalette.Text, Qt.darkGreen)
+        comp.setPalette(palette)
+        self.refresh_app()
+        pass
+
+
+    def set_gray_text(self, comp):
+        palette = QPalette()
+        palette.setColor(QPalette.Text, Qt.lightGray)
+        comp.setPalette(palette)
+        self.refresh_app()
+        pass
+
     def refresh_app(self):
         qApp.processEvents()
+
+
+    def sin_checked_printer(self):
+        if self.checkBox_printer.isChecked():
+            self.csm_helper.printer_is_checked = True
+        else:
+            self.csm_helper.printer_is_checked = False
+
+    def sin_checked_currenter(self):
+        if self.checkBox_currenter.isChecked():
+            self.csm_helper.currenter_is_checked = True
+        else:
+            self.csm_helper.currenter_is_checked = False
+
+    def sin_checked_cc2640(self):
+        if self.checkBox_cc2640.isChecked():
+            self.csm_helper.cc2640_is_checked = True
+        else:
+            self.csm_helper.cc2640_is_checked = False
+
+    def sin_checked_csm3510(self):
+        print("点击-sin_checked_csm3510")
+        if self.checkBox_csm3510.isChecked():
+            self.csm_helper.csm3510_is_checked = True
+        else:
+            self.csm_helper.csm3510_is_checked = False
+
     def sin_str_call(self, info):
         self.lineEdit_result.setText(info)
     def sin_dis_str_call(self, info):
         self.plainText_display.appendPlainText(info)
     def sin_log_str_call(self,info):
-        self.textBrowser_log.insertHtml(info)
+        self.plainTextEdit_log.appendPlainText(info)
     def reset_button_clicked(self):
         pass
+    def sin_result_int_call(self, value):
+        if value == self.csm_helper.test_PASS:
+            self.textBrowser_reuslt.setSource(QUrl("message_pass.html"))
+        elif value == self.csm_helper.test_FAIL:
+            self.textBrowser_reuslt.setSource(QUrl("message_fail.html"))
+        elif value == self.csm_helper.test_WAIT:
+            self.textBrowser_reuslt.setSource(QUrl("waitting.html"))
+            self.plainText_display.clear()
+            self.plainText_display.appendPlainText("等待中......")
+        self.refresh_app()
 
     def csm3510_btn_setting_clicked(self):
-        pass
+        self.set_gray_text(self.csm3510_head_text)
         self.csm_helper.csm3510.port = self.csm3510_comboBox_port.currentText()
-        self.csm3510_head_text.setAutoFillBackground(True)
+        self.csm_helper.csm3510.baudrate = self.csm3510_comboBox_baudrate.currentText()
+        self.csm_helper.csm3510.databits = self.csm3510_comboBox_databits.currentText()
         QMessageBox.information(self, "提示", "更新成功-->" + self.csm_helper.csm3510.port, QMessageBox.Yes)
-        palette = QPalette();palette.setColor(QPalette.Text, Qt.green)
-        self.csm3510_head_text.setPalette(palette)
+        self.set_green_text(self.csm3510_head_text)
 
     def cc2640_btn_setting_clicked(self):
-        pass
+        self.set_gray_text(self.cc2640_head_text)
+        self.csm_helper.cc2640.port = self.cc2640_comboBox_port.currentText()
+        self.csm_helper.cc2640.baudrate = self.cc2640_comboBox_baudrate.currentText()
+        self.csm_helper.cc2640.databits = self.cc2640_comboBox_databits.currentText()
+        QMessageBox.information(self, "提示", "更新成功-->" + self.csm_helper.cc2640.port, QMessageBox.Yes)
+        self.set_green_text(self.cc2640_head_text)
 
     def printer_btn_setting_clicked(self):
-        pass
+        self.set_gray_text(self.printer_head_text)
+        self.csm_helper.printer.port = self.printer_comboBox_port.currentText()
+        self.csm_helper.printer.baudrate = self.printer_comboBox_baudrate.currentText()
+        self.csm_helper.printer.databits = self.printer_comboBox_databits.currentText()
+        QMessageBox.information(self, "提示", "更新成功-->" + self.csm_helper.printer.port, QMessageBox.Yes)
+        self.set_green_text(self.printer_head_text)
 
     def currenter_btn_setting_clicked(self):
-        pass
+        self.set_gray_text(self.currenter_head_text)
+        self.csm_helper.currenter.port = self.currenter_comboBox_port.currentText()
+        self.csm_helper.currenter.baudrate = self.currenter_comboBox_baudrate.currentText()
+        self.csm_helper.currenter.databits = self.currenter_comboBox_databits.currentText()
+        QMessageBox.information(self, "提示", "更新成功-->" + self.csm_helper.currenter.port, QMessageBox.Yes)
+        self.set_green_text(self.currenter_head_text)
 
     def btn_printer_autodetect_call(self):
-        pass
+        self.set_gray_text(self.printer_head_text)
         self.btn_printer_autodetect.setText("识别中...")
         self.btn_printer_autodetect.setEnabled(False)
         self.refresh_app()
@@ -73,8 +173,10 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.printer_comboBox_databits.clear()
                 self.printer_comboBox_databits.addItem("8")
                 self.textBrowser_reuslt.setSource(QUrl("waitting.html"))
-                QMessageBox.information(self, "提示", "成功检测CC2640串口-->"+port, QMessageBox.Yes)
+                self.set_green_text(self.printer_head_text)
+                QMessageBox.information(self, "提示", "成功检测打印机串口-->"+port, QMessageBox.Yes)
             else:
+                QMessageBox.information(self, "提示", "识别失败，请检测连线", QMessageBox.Yes)
                 self.plainText_display.appendPlainText("识别失败，请检查连线: " + port)
             self.btn_printer_autodetect.setText("自动识别")
             self.btn_printer_autodetect.setEnabled(True)
@@ -82,6 +184,8 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             print(str(e))
 
     def btn_currenter_autodetect_call(self):
+
+        self.set_gray_text(self.currenter_head_text)
         self.btn_currenter_autodetect.setText("识别中...")
         self.btn_currenter_autodetect.setEnabled(False)
         self.refresh_app()
@@ -96,15 +200,19 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.currenter_comboBox_databits.clear()
                 self.currenter_comboBox_databits.addItem("8")
                 self.textBrowser_reuslt.setSource(QUrl("waitting.html"))
-                QMessageBox.information(self, "提示", "成功检测CC2640串口-->"+port, QMessageBox.Yes)
+                self.set_green_text(self.currenter_head_text)
+                QMessageBox.information(self, "提示", "成功检测电流表串口-->"+port, QMessageBox.Yes)
             else:
+                QMessageBox.information(self, "提示", "识别失败，请检测连线", QMessageBox.Yes)
                 self.plainText_display.appendPlainText("识别失败，请检查连线: " + port)
             self.btn_currenter_autodetect.setText("自动识别")
             self.btn_currenter_autodetect.setEnabled(True)
         except Exception as e:
             print(str(e))
 
+
     def btn_cc2640_autodetect_call(self):
+        self.set_gray_text(self.cc2640_head_text)
         self.btn_cc2640_autodetect.setText("识别中...")
         self.btn_cc2640_autodetect.setEnabled(False)
         self.refresh_app()
@@ -118,8 +226,11 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             self.cc2640_comboBox_databits.clear()
             self.cc2640_comboBox_databits.addItem("8")
             self.textBrowser_reuslt.setSource(QUrl("waitting.html"))
+            self.set_green_text(self.cc2640_head_text)
             QMessageBox.information(self, "提示", "成功检测CC2640串口-->"+port, QMessageBox.Yes)
+
         else:
+            QMessageBox.information(self, "提示", "识别失败，请检测连线", QMessageBox.Yes)
             self.plainText_display.appendPlainText("识别失败，请检查连线: " + port)
         self.btn_cc2640_autodetect.setText("自动识别")
         self.btn_cc2640_autodetect.setEnabled(True)
@@ -128,6 +239,7 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def btn_csm3510_autodetect_call(self):
         """CSM3510自动识别攒看函数"""
+        self.set_gray_text(self.csm3510_head_text)
         self.btn_csm3510_autodetect.setText("识别中...")
         self.btn_csm3510_autodetect.setEnabled(False)
         self.refresh_app()
@@ -141,17 +253,30 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             self.csm3510_comboBox_databits.clear()
             self.csm3510_comboBox_databits.addItem("8")
             self.textBrowser_reuslt.setSource(QUrl("waitting.html"))
+            self.set_green_text(self.csm3510_head_text)
             QMessageBox.information(self, "提示", "成功检测CSM3510串口-->"+port, QMessageBox.Yes)
+            self.csm_helper.csm3510.is_available = True
+
         else:
+            QMessageBox.information(self, "提示", "识别失败，请检测连线", QMessageBox.Yes)
             self.plainText_display.appendPlainText("识别失败，请检查连线: " + port)
         self.btn_csm3510_autodetect.setText("自动识别")
         self.btn_csm3510_autodetect.setEnabled(True)
 
 
 class CSM3510_Helper(QThread):
+      # const
+      test_PASS = 0x01; test_FAIL = 0x02;test_WAIT = 0x03
+      # flag
+      had_test_flag = False;
+      csm3510_is_checked = True; cc2640_is_checked = True; currenter_is_checked = True; printer_is_checked = True;
+      #single-slot
       sin_str = pyqtSignal(str)
-      sin_dis_str = pyqtSignal(str)
       sin_log_str = pyqtSignal(str)
+      sin_dis_str = pyqtSignal(str)
+      sin_serial_lost_str = pyqtSignal(bool)
+      sin_result_int = pyqtSignal(int)
+      # object
       csm3510 = CSM3510()
       cc2640  = CC2640()
       printer = QrPrinter()
@@ -164,17 +289,48 @@ class CSM3510_Helper(QThread):
               # 串口工作主流程
               while True:
                   pass
-                  time.sleep(2)
-                  print(self.csm3510.port)
-              # name = self.csm3510.find_port()
-              # self.sin_str.emit(name)
-              # self.sin_dis_str.emit("找到CSM3510关联串口"+name)
-              # self.sin_log_str.emit("<h5>找到csm3510串口</h5>")
+                  time.sleep(0.2)
+                  if self.cc2640_is_checked==False and self.printer_is_checked==False and self.currenter_is_checked==False:
+                      if self.csm3510.is_available == True:
+                          result = self.csm3510.query_work_state()
+                          if result == True:
+                               if self.had_test_flag == False:
+                                   self.had_test_flag = True
+                                   self.print_log("找到CSM3510"); print("CSM3510在广播状态");self.print_dis("搜索到CSM3510")
+                                   result, mac =  self.csm3510.get_mac_address()
+                                   self.print_dis("1. 获取mac地址:"+str(result)+"->"+ mac);self.print_log("mac地址:" + str(result) + "->" + mac)
+                                   if result == True:
+                                       print("成功获取mac地址", mac)
+                                       result, version = self.csm3510.get_soft_version()
+                                       self.print_dis("2. 获取版本号:" +str(result)+"->"+ version);self.print_log("版本号:" +str(result)+"->"+ version)
+                                       if result==True:
+                                           print("成功获取版本号:",version)
+                                           self.print_dis("请拿下模块CSM3510")
+                                           self.print_result(self.test_PASS)
+                                   else:
+                                       self.print_result(self.test_FAIL)
+                               else:
+                                   pass
+
+                          else:
+                            self.had_test_flag = False
+                            print("未找到CSM3510")
+                            self.print_result(self.test_WAIT)
+                  else:
+                    pass
+                    self.print_result(self.test_WAIT)
           except Exception as e:
               print(str(e))
 
-        # while True:
-        #     pass
+      def print_log(self, info):
+          self.sin_log_str.emit(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"=>"+info)
+
+      def print_dis(self, info):
+          self.sin_dis_str.emit(info)
+
+      def print_result(self, const):
+          self.sin_result_int.emit(const)
+
       def mainloop_app(self):
           try:
               pass
--- CSM3510_Helper/chipsea_tools.py
+++ CSM3510_Helper/chipsea_tools.py
@@ -4,9 +4,141 @@ import  time
 
 class CSM3510(object):
     """docstring for ClassName"""
+    is_available = False
     port = "com1"
+    baudrate = 9600
+    databits = 8
     cmd_wakeup = [0x10,0x00,0x00,0x00]
     cmd_query_state = [0x10,0x00,0x00,0xC5,0x01,0x81,0x45]
+    cmd_get_mac_address =[0x10,0x00,0x00,0xC5,0x01,0x79,0xBD]
+    cmd_get_soft_version = [0x10,0x00,0x00,0xC5,0x01,0x78,0xBC]
+    cmd_set_adv_37 =[0x10,0x00,0x00,0xC5,0x01,0xf7,0x33]
+    cmd_set_adv_38 =[0x10,0x00,0x00,0xC5,0x01,0xf8,0x3c]
+    cmd_set_adv_39 =[0x10,0x00,0x00,0xC5,0x01,0xf9,0x3D]
+    cmd_set_adv_default = [0x10,0x00,0x00,0xC5,0x01,0xf6,0x32]
+    cmd_send_notify_data = [0x10,0x00,0x00,0xC5,0x15,0x94,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x00,0x44]
+    cmd_set_force_sleep = [0x10,0x00,0x00,0xC5,0x01,0x74,0xB0]
+
+    def setting_command(self, cmd):
+        try:
+            ser = serial.Serial()
+            ser.port  = self.port
+            ser.baudrate = self.baudrate
+            ser.databits = self.databits
+            ser.timeout = 1
+            ser.open()
+            ser.flushInput()
+            ser.flushOutput()
+            ser.write(cmd)
+            rec = ser.read(4)
+            ser.close()
+            if rec:
+                print("成功接收到数...", ' '.join('{:02x}'.format(x) for x in rec))
+                print("命令执行成功")
+                if rec[0]==0xc5 and rec[1]==0x01 and rec[2]==0x80 and rec[3]==0x44:
+                    return True
+            else:
+                return False
+        except Exception as e:
+            pass
+            return False
+
+    def get_soft_version(self):
+        try:
+            ser = serial.Serial()
+            ser.port  = self.port
+            ser.baudrate = self.baudrate
+            ser.databits = self.databits
+            ser.timeout = 1
+            ser.open()
+            ser.flushInput()
+            ser.flushOutput()
+            print("发送数据", ' '.join('{:02x}'.format(x) for x in self.cmd_get_soft_version))
+            ser.write(self.cmd_get_soft_version)
+            rec = ser.read(4)
+            ser.close()
+            if rec:
+                print("成功接收到数...", ' '.join('{:02x}'.format(x) for x in rec))
+                if rec[0] == 0xc5 and rec[1]==0x01 and rec[2] == 0x23 :
+                    return True, "CS2.3"
+                else:
+                    return False, "Erro"
+            else:
+                return False, "None"
+        except Exception as e:
+            pass
+            return False, "None"
+
+    def get_mac_address(self):
+        try:
+            ser = serial.Serial()
+            ser.port  = self.port
+            ser.baudrate = self.baudrate
+            ser.databits = self.databits
+            ser.timeout = 1
+            ser.open()
+            ser.flushInput()
+            ser.flushOutput()
+            print("发送数据", ' '.join('{:02x}'.format(x) for x in self.cmd_get_mac_address))
+            ser.write(self.cmd_get_mac_address)
+            rec = ser.read(9)
+            ser.close()
+            if rec:
+                print("成功接收到数...", ' '.join('{:02x}'.format(x) for x in rec))
+                mac = rec[2:8]
+                if mac[0] == 0xc8 and mac[1] == 0xb2:
+                    address = ' '.join('{:02x}'.format(x) for x in mac)
+                    return True, address.upper()
+
+                else:
+                    address = ' '.join('{:02x}'.format(x) for x in mac)
+                    return  False, address.upper()
+            else:
+                return False, "None"
+        except Exception as e:
+            pass
+            return False, "None"
+
+    def read_app_data(self):
+        try:
+            ser = serial.Serial()
+            ser.port  = self.port
+            ser.baudrate = self.baudrate
+            ser.databits = self.databits
+            ser.timeout = 1
+            ser.open()
+            rec = ser.read(20)
+            ser.close()
+            if rec:
+                print("成功接收到数...",' '.join('{:02x}'.format(x) for x in rec))
+                if rec[0] == 0x01 and rec[19] == 0x00:
+                    return True
+        except Exception as e:
+            pass
+            return False
+
+    def query_work_state(self):
+        try:
+            ser = serial.Serial()
+            ser.port  = self.port
+            ser.baudrate = self.baudrate
+            ser.databits = self.databits
+            ser.timeout = 0.1
+            ser.open()
+            ser.flushInput()
+            ser.flushOutput()
+            print("发送数据", ' '.join('{:02x}'.format(x) for x in self.cmd_query_state))
+            ser.write(self.cmd_query_state)
+            rec = ser.read(4)
+            ser.close()
+            if rec:
+                print("成功接收到数...", ' '.join('{:02x}'.format(x) for x in rec))
+                if(rec[0]==0xc5 and rec[1]==0x01 and rec[2]==0x01 and rec[3]==0xc5):
+                    return True
+        except Exception as e:
+            print("串口丢失")
+            return False
+
     def find_port(self):
         pass
         ports = serial.tools.list_ports.comports()
@@ -39,6 +171,8 @@ class CSM3510(object):
 class CC2640(object):
     """docstring for ClassName"""
     port = "com1"
+    baudrate = 9600
+    databits = 8
     cmd_wakeup = [0x10,0x00,0x00,0x00]
     cmd_query_state = [0x10,0x00,0x00,0xC5,0x01,0x81,0x45]
     def find_port(self):
@@ -72,6 +206,8 @@ class CC2640(object):
 class QrPrinter(object):
     """docstring for ClassName"""
     port = "com1"
+    baudrate = 9600
+    databits = 8
     cmd_wakeup = [0x10,0x00,0x00,0x00]
     cmd_query_state = [0x10,0x00,0x00,0xC5,0x01,0x81,0x45]
     def find_port(self):
@@ -105,6 +241,8 @@ class QrPrinter(object):
 class CurrentMeasure(object):
     """docstring for ClassName"""
     port = "com1"
+    baudrate = 9600
+    databits = 8
     cmd_wakeup = [0x10,0x00,0x00,0x00]
     cmd_query_state = [0x10,0x00,0x00,0xC5,0x01,0x81,0x45]
     def find_port(self):
